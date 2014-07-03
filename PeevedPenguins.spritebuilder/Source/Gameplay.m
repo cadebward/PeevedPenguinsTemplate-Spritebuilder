1b1f9ab6b540e2ce6e947ebcf8d1366cb8f894d0
Cade Ward <cadew@live.com>
2014-07-03 14:05:41 -0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,15 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Cade on 7/3/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay {
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+@end
